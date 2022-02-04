a4d68df4318ca2badba42e15efc593a47f11a83c
isaacledesma76 <isaacledesma76@gmail.com>
2022-02-03 20:18:07 -0600

    actividad 8 cuestionario

---
+++ Actividad-7/ejercicio-funciones-enclase.js
@@ -0,0 +1,12 @@
+var iva = 16
+var preciomovil = 200
+var accesorio = 50
+var presupuesto = 3000
+var cuentabanco = 5000
+
+function CompraTotal ()
+for (i = 0; i < cuentabanco; i++) {
+    i + (preciomovil + accesorio) 
+     
+}
+console.log(i)
